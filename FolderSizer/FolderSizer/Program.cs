0ec3922fe7bf1d3f918a750289958ea1e7e763a0
Cuc Denis <48867203+CucDenis@users.noreply.github.com>
2020-04-14 21:39:01 +0300

    Add files via upload

---
+++ FolderSizer/FolderSizer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace FolderSizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write the name of the folder where you have the file you want to know the size: ");
+            string path = Console.ReadLine();
+
+            DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (var fi in di.GetFiles()) {
+                Console.WriteLine("File {0} with size of {1} bytes",fi.Name,fi.Length);
+            }
+        }
+    }
+}
