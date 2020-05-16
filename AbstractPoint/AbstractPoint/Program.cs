384078bd40c2afe9613fda361af283973883f0db
Cuc Denis <48867203+CucDenis@users.noreply.github.com>
2020-05-16 10:35:37 +0300

    Update Program.cs

--- AbstractPoint/AbstractPoint/Program.cs
+++ AbstractPoint/AbstractPoint/Program.cs
@@ -6,9 +6,17 @@ namespace AbstractPoint
     {
         static void Main(string[] args)
         {
-            var punct1 = new Point("Rectangular",34,60);
+             var punct1 = new Point("Rectangular",34,60);
             var punct2 = new Point("Polar",30,60);
 
+            Console.WriteLine(punct1.ToString());
+            Console.WriteLine(punct2.ToString());
+
+            punct2.Move(50,67);
+            punct2.Rotate(60);
+
+            Console.WriteLine(punct1.ToString());
+            Console.WriteLine(punct2.ToString());
         }
     }
 }
