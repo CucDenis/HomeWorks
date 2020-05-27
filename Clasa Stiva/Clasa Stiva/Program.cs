b9b5d1b9ed0b7e90eae82c5860ae7b67ae5f496e
Cuc Denis <48867203+CucDenis@users.noreply.github.com>
2020-05-27 21:28:10 +0300

    Add files via upload

---
+++ Clasa Stiva/Clasa Stiva/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Clasa_Stiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stiva stiva = new Stiva(5);
+            Stiva stivaNoua = new Stiva(5);
+
+            stiva.Push(5);
+            stiva.Push(6);
+            stiva.Push(5);
+            stiva.Push(6);
+
+            stiva.Push("informatica");
+            stiva.Push("antagonist");
+
+            stivaNoua.Push(1);
+            stivaNoua.Push(2);
+            stivaNoua.Push(3);
+            stivaNoua.Push("macaroane");
+            stivaNoua.Push("Decameronul");
+
+            for (int i=0; i < 4; i++)
+            {
+                Console.WriteLine(stiva.Pop());
+                Console.WriteLine(stiva.Pop("string"));
+            }
+
+            stivaNoua.Clear();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(stiva.Pop());
+                Console.WriteLine(stiva.Pop("string"));
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(stivaNoua.Pop());
+            }
+            
+        }
+    }
+}
---
+++ Clasa Stiva/Clasa Stiva/Stiva.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clasa_Stiva
+{
+    class Stiva
+    {
+        private List<int> stivaI = new List<int>();
+        private List<string> stivaS = new List<string>();
+        private int lungime;
+        private int MAX = 1000000;
+        private int i;
+
+
+        public List<int> P_IStiva
+        {
+            get
+            {
+                return stivaI;
+            }
+            set
+            {
+                stivaI = value;
+            }
+        }
+
+        public List<string> P_SStiva
+        {
+            get
+            {
+                return stivaS;
+            }
+            set
+            {
+                stivaS = value;
+            }
+        }
+
+
+        public Stiva(int lungimeStiva)
+        {
+            if (lungimeStiva < MAX)
+            {
+                lungime = lungimeStiva;
+            }else
+            {
+                Console.WriteLine("Lungime stiva depasita.\n" +
+                                  "Lungime stiva maxima: 1000000");
+            }
+        }
+
+        public void Push(int valoare)
+        {
+            P_IStiva.Add(valoare);
+        }
+
+        public void Push(string valoare)
+        {
+            P_SStiva.Add(valoare);
+        }
+
+        public string Pop()
+        {
+            try
+            {
+                if (P_IStiva.Count != 0)
+                {
+                    string valoareActuala = (P_IStiva[P_IStiva.Count - 1]).ToString();
+                    P_IStiva.RemoveAt(P_IStiva.Count - 1);
+                    return valoareActuala;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("A aparut aceasta exceptie: {0}", e);
+            }
+            return "\t"; ;
+        }
+
+        public string Pop(string tipStiva)
+        {
+            if(tipStiva == "string")
+            {
+                try
+                {
+                    if (P_SStiva.Count != 0)
+                    {
+                        string valoareActuala = (P_SStiva[P_SStiva.Count - 1]).ToString();
+                        P_SStiva.RemoveAt(P_SStiva.Count - 1);
+                        return valoareActuala;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("A aparut aceasta exceptie: {0}", e);
+                }
+            }
+            return "\t";
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < P_IStiva.Count; i++)
+            {
+                P_IStiva.Remove(P_IStiva[i]);
+            }
+        }
+
+        public void Clear(string tipStiva)
+        {
+            if (tipStiva == "string")
+            {
+                for (int i = 0; i < P_SStiva.Count; i++)
+                {
+                    P_SStiva.Remove(P_SStiva[i]);
+                }
+            }
+        }
+    }
+}
