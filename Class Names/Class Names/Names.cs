73b3717969fe28f1129e13a6adc3fda1c5bef36c
Cuc Denis <48867203+CucDenis@users.noreply.github.com>
2020-06-05 09:44:17 +0300

    Add files via upload

---
+++ Class Names/Class Names/Names.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Names
+{
+    class Names
+    {
+        private string[] _listaNume = new string[lungime];
+        private static int lungime = 10;
+        private int i = 0;
+
+
+        public string this[int index]      
+        {
+            get
+            {
+                string tmp = "";
+                try
+                {
+                    if (index >= 0 && index <= ListaNume.Length - 1)
+                    {
+                        tmp = ListaNume[index];
+                    }
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    tmp = e.Message;
+                }
+
+                return (tmp);
+            } 
+            set
+            {
+                ListaNume[index] = value;
+            }
+        }
+
+
+        public int this[string name]
+        {
+            get
+            {
+                int index = 0;
+
+                while (index < ListaNume.Length)
+                {
+                    if (ListaNume[index] == name)
+                    {
+                        return index;
+                    }
+                    index++;
+                }
+                return index;
+            }
+
+        }
+
+
+        public string[] ListaNume
+        {
+            get
+            {
+                return _listaNume;
+            }
+
+            set
+            {
+                _listaNume = value;
+            }
+        }
+
+
+        public Names()
+        {
+            for (int index = 0; index < ListaNume.Length; index++)
+            {
+                ListaNume[index] = "Nume Nou";
+            }
+        }
+
+        public void Add(string name)
+        {
+            if (i >= 0 && i < ListaNume.Length)
+            {
+                ListaNume[i] = name;
+                i++;
+            }
+        }
+
+        public void Afisare()
+        {
+            foreach (var item in ListaNume)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ Class Names/Class Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Class_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaPersoane = new Names();
+            listaPersoane.Add("Andrei Marius");
+            listaPersoane.Add("Andrei Flavius");
+            listaPersoane.Add("Marta");
+            listaPersoane.Add("Urs Radu");
+            listaPersoane.Add("Veres Rares");
+            listaPersoane.Afisare();
+
+            Console.WriteLine(listaPersoane[2]);
+            Console.WriteLine(listaPersoane[1132]);
+            Console.WriteLine(listaPersoane["Marta"]);
+        }
+    }
+}
